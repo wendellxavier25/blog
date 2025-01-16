b72f16fab7f7b09a5fd4b2c95b7d80849acbe5e7
wendellxavier25 <wendellx25@gmail.com>
2025-01-16 08:31:54 -0300

    models post

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Category, Page
+from blog.models import Tag, Category, Page, Post
 
 @admin.register(Tag)
 class AdminTag(admin.ModelAdmin):
@@ -28,4 +28,17 @@ class PageAdmin(admin.ModelAdmin):
     list_filter = ('is_published',)
     list_editable = ('is_published',)
     ordering = ('-id',)
+    prepopulated_fields = {"slug": ('title',)}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'is_published')
+    list_display_links = ('title',)
+    search_fields = ('id', 'title', 'slug',)
+    list_per_page = 50
+    list_filter = ('category', 'is_published',)
+    list_editable = ('is_published',)
+    ordering = ('-id',)
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by', )
     prepopulated_fields = {"slug": ('title',)}
--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils.rands import slugify_new
+from django.contrib.auth.models import User
 
 
 
@@ -57,4 +58,33 @@ class Page(models.Model):
     
     def __str__(self):
         return self.title
+    
 
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, default="", null=False, blank=True, max_length=150)
+    exerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(default=False)
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default='')
+    cover_in_post_content = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='post_created_by')
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='post_updated_by')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    tags = models.ManyToManyField(Tag, blank=True, default='')
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
