d95ce471c9f1ad7da031a98b3a683b4e7701a280
wendellxavier25 <wendellx25@gmail.com>
2025-01-22 08:50:36 -0300

    add category, created_by

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from blog.models import Tag, Category, Page, Post
 from django_summernote.admin import SummernoteModelAdmin
+from django.urls import  reverse
+from django.utils.safestring import mark_safe
 
 @admin.register(Tag)
 class AdminTag(admin.ModelAdmin):
@@ -42,9 +44,17 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('category', 'is_published',)
     list_editable = ('is_published',)
     ordering = ('-id',)
-    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by', )
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by', 'link')
     prepopulated_fields = {"slug": ('title',)}
     autocomplete_fields = ('tags', 'category')
+
+    def link(self, obj):
+        if not obj.pk:
+            return ''
+        url_do_post = obj.get_absolute_url()
+        safe_link = mark_safe(f'<a target="_blank" href="{url_do_post}">Ver Post: {obj.title}</a>')
+
+        return safe_link
 
 
     def save_model(self, request, obj, form, change):
